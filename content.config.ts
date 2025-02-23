358233d897ae98325098be33abe93a6f4b6627ba
leoboyerbx <contact@leoboyer.dev>
2025-02-23 15:28:06 +0100

    wip on content migration

--- content.config.ts
+++ content.config.ts
@@ -10,5 +10,31 @@ export default defineContentConfig({
         title: z.string(),
       }),
     }),
+    projects: defineCollection({
+      type: 'page',
+      source: 'projects/**/*.md',
+      schema: z.object({
+          locale: z.string(),
+          status: z.enum(['draft', 'published']),
+          name: z.string(),
+        slug: z.string(),
+        baseline: z.string(),
+        createdAt: z.date(),
+        updatedAt: z.date(),
+        links: z.array(z.object({
+            name: z.string(),
+            url: z.string(),
+        })),
+        thumbnail: z.string().editor({ input: 'media' }),
+        skillsTitle: z.string(),
+        skills: z.array(z.string()),
+        video: z.object({
+          id: z.number(),
+          title: z.string(),
+          embed: z.string(),
+          description: z.string(),
+        }),
+      }),
+    }),
   },
 })
